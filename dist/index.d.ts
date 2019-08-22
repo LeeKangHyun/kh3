76f68a74bc7de848484cdfa35415a31e72ffcc28
kanghyunlee <rkdgusdl92@gmail.com>
2019-08-22 23:07:55 +0900

    Log 찍기

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare const LOG: (msg: string) => void;
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LOG = function (msg) {
+    console.log(msg);
+};
